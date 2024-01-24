5f8ae7b764d33a3cf6d236e9fe5ca37af659bb9b
Timothy Jones <timothy.l.jones@gmail.com>
2024-01-24 21:56:12 +1100

    fix: include all dependencies in jsii package

---
+++ ci/build-and-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eu
+set -eu # We do this twice so that winadows github runners will properly read them
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running
+# shellcheck source=lib/robust-bash.sh
+. "$SCRIPT_DIR"/lib/robust-bash.sh 
+
+npm install # We can't do npm ci because lerna has optional dependencies :(
+
+npx lerna --ci run format:check
+npx lerna --ci run build
+npx lerna --ci run lint
+npx lerna --ci run test
+npx lerna --ci run test:verify
+
